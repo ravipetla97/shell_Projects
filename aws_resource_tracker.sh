f647e9447417c04c3b6dcc439954f80cecb1c95f
Ubuntu <ubuntu@ip-172-31-37-110.eu-north-1.compute.internal>
2023-07-31 12:41:22 +0000

    code to get the output for the required fields

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -5,27 +5,32 @@
 # Author : Ravi Petla
 # Date :31st  july 2023
 # version v1
-# this script tracks all your organisation  aws accountsso that you can 
-  report to any dashboard....
+# this script tracks all your organisation  aws accountsso that you can  report to any dashboard....
 ###################################### 
 
 ## we will track the following resources
 # aws ec2
 # aws s3 buckets
 # aws iam roles
-# aws lambada functions
+# aws lambda functions
 
 #########################################
 
 
 # list of s3 buckets
-aws s3 ls
+echo "print list of s3 buckets::::::::::::"
+aws s3 ls 
 
 #list of ec2 instances
-aws ec2 describe instances
+echo "print list of ec2 instances::::::::"
+aws ec2 describe-instances | jq '[.Reservations[].Instances[].InstanceId,.Reservations[].Instances[].Tags[].Key,.Reservations[].Instances[].Tags[].Value]'
 
-#list of lambada functions
-aws lambada list-functions
+#list of lambda functions
+echo "print list lambda functions::::"
+aws lambda list-functions
 
 #list of iam users
-aws iam list-users
+echo "print list of iam users::::::::"
+aws iam list-users | jq '[.Users[].UserName,.Users[].UserId']
+
+
